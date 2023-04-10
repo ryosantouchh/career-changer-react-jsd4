9885cd456be0ac035723bc1548bdaea224f704d9
ryosantouchh <kittitat.suntimak@gmail.com>
2023-04-10 08:54:23 +0700

    completed

--- 03-react-life-cycle-hook/tutorial-02-react-component/02-react-app/src/App.jsx
+++ 03-react-life-cycle-hook/tutorial-02-react-component/02-react-app/src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: 30,
+    };
+    this.handleClickIncrement = this.handleClickIncrement.bind(this);
+  }
+
+  handleClickIncrement() {
+    // how did 'prev' know this.state ?? what's behind the scene ??
+    this.setState((prevState) => ({
+      temperature: prevState.temperature + 1,
+    }));
+  }
+
+  // lexically scoped >> refer parent
+  handleClickDecrement = () => {
+    if (this.state.temperature > 0) {
+      this.setState({ temperature: this.state.temperature - 1 });
+      // shallow merge
+    }
+  };
+
   render() {
     return (
       <div id="app">
-        <Header />
-        <Content />
-        <Footer />
+        <Header temperature={this.state.temperature} name="T" />
+        {/* this.props = { temperature : this.state.temperature, name: "T"  } */}
+        <Content temperature={this.state.temperature} />
+        <Footer
+          handleClickIncrement={this.handleClickIncrement}
+          handleClickDecrement={this.handleClickDecrement}
+        />
+        <Challenge temperature={this.state.temperature} />
       </div>
     );
   }
@@ -16,36 +44,90 @@ class App extends React.Component {
 
 class Header extends React.Component {
   render() {
+    const { temperature } = this.props;
+
     return (
-      // Code here
-      // <Header />
+      <header>
+        <span>Turn on / off</span>
+        <p>Current Temperature: {temperature}</p>
+      </header>
     );
   }
 }
 
 class Content extends React.Component {
   render() {
+    const { temperature } = this.props;
+
     return (
-      // Code here
-      // <Content />
+      <main>
+        <Temperature temperature={temperature} />
+      </main>
     );
   }
 }
 
 class Temperature extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { temperature } = this.props;
+
     return (
-      // Code here
-      // <Temperature />
+      <div id="temperature">
+        <span>{temperature} Oc</span>
+      </div>
     );
   }
 }
 
 class Footer extends React.Component {
   render() {
+    const { handleClickIncrement, handleClickDecrement } = this.props;
+    // this.props = { handleClickIncrement : ... , handleClickDecrement : ... }
+
     return (
-      // Code here
-      // <Footer />
+      <footer>
+        <button
+          style={{ backgroundColor: "lightgreen" }}
+          onClick={handleClickIncrement}
+        >
+          Up
+        </button>
+        <button
+          style={{ backgroundColor: "yellow" }}
+          onClick={handleClickDecrement}
+        >
+          Down
+        </button>
+      </footer>
+    );
+  }
+}
+
+class Challenge extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Touch",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ textAlign: "center" }}>
+          <h2>Challenge by : {this.state.name}</h2>
+          <span>
+            {this.props.temperature <= 23 ? "ประเทศไทยร้อนชิบหาย!!" : false}
+            {this.props.temperature >= 27
+              ? "แน่ใจหรอว่าจะเปิดแอร์แบบนี้ที่ประเทศไทย"
+              : false}
+          </span>
+        </div>
+      </div>
     );
   }
 }
